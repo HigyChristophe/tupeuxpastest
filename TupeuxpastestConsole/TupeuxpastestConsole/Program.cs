dcdf948625e819c280bfccfbaa74e49da8b24597
HigyChristophe <christophe.higy@outlook.fr>
2016-11-22 15:19:06 +0100

    Initialisation

---
+++ TupeuxpastestConsole/TupeuxpastestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupeuxpastestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vive les soirées pizza-coca !!");
+            Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
